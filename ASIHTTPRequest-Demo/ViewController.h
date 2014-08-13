c8ecbc7e1b8f33ac698986179102f499c5e2dddd
Jakey <i@skyfox.org>
2014-08-13 14:22:28 +0800

    Initial Commit

---
+++ ASIHTTPRequest-Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ASIHTTPRequest-Demo
+//
+//  Created by Jakey on 14-8-13.
+//  Copyright (c) 2014年 www.skyfox.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ASIHTTPRequest-DemoTests/ASIHTTPRequest_DemoTests.m
@@ -0,0 +1,34 @@
+//
+//  ASIHTTPRequest_DemoTests.m
+//  ASIHTTPRequest-DemoTests
+//
+//  Created by Jakey on 14-8-13.
+//  Copyright (c) 2014年 www.skyfox.org. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ASIHTTPRequest_DemoTests : XCTestCase
+
+@end
+
+@implementation ASIHTTPRequest_DemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
